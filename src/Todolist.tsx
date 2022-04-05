4417c3da997a00c479cc38b83ce95a99311483f8
UlianaOr <Ouliana9@gmail.com>
2022-04-05 14:34:58 +0200

    todo

---
+++ src/Todolist.tsx
@@ -0,0 +1,24 @@
+import React from "react";
+
+const Todolist=()=>{
+    return(
+        <div>
+                <h1>What to learn</h1>
+                <div>
+                    <input/>
+                    <button>+</button>
+                </div>
+                <ul>
+                    <li><input type="checkbox" checked={true}/> <span>HTML&CSS</span></li>
+                    <li><input type="checkbox" checked={true}/> <span>JS</span></li>
+                    <li><input type="checkbox" checked={false}/> <span>React</span></li>
+                </ul>
+                <div>
+                    <button>All</button>
+                    <button>Active</button>
+                    <button>Completed</button>
+                </div>
+            </div>
+    )
+
+}
