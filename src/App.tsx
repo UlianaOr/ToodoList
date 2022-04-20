c98e361f5cc934a076f288804bc6fa9baf570155
UlianaOr <Ouliana9@gmail.com>
2022-04-20 13:01:26 +0200

    qwe

--- src/App.tsx
+++ src/App.tsx
@@ -3,17 +3,25 @@ import './App.css';
 import { Todolist } from './Todolist';
 
 function App() {
-    const tasks1  = [
+//let tasks1  = [
+//{id: 1, title: "HTML&CSS", isDone: true},
+      //  {id: 2, title: "JS", isconstDone: true},
+      //  {id: 3, title: "ReactJS", isDone: false},
+      //  {id: 4, title: "Rest API", isDone: false},
+       
+    //]
+
+    const [tasks1, setTasks1]= useState ([
         {id: 1, title: "HTML&CSS", isDone: true},
-        {id: 2, title: "JS", isDone: true},
+        {id: 2, title: "JS", isconstDone: true},
         {id: 3, title: "ReactJS", isDone: false},
         {id: 4, title: "Rest API", isDone: false},
        
-    ]
+    ])   
 
     
-    const removeTask=(id:number)=>{
-        console.log(id)
+    const removeTask=(newid: number)=>{
+        tasks1 =tasks1.filter((el.))
     }
     return (
         <div className="App">
