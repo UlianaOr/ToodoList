3ca1eb65bd13295101a59653d8c7918c9eca00c3
UlianaOr <Ouliana9@gmail.com>
2022-09-22 11:17:47 +0200

    newArray

--- src/App.tsx
+++ src/App.tsx
@@ -14,18 +14,28 @@ type TodoListType= {
 
 function App() {
 
-    let [todolist, setTodolist]=useState<Array<TodoListType>> ([
-        {id:v1(),title:'What to learn', filter:'all'},
-        {id:v1(),title:'What to buy', filter:'completed'},
+    let [todolists, setTodolists] = useState<Array<todoListsType>> ([
+        {id:todolistID1, title:'What to learn', filter:'all'},
+        {id:todolistID2,title:'What to buy', filter:'completed'},
     ])
 
-    let [tasks, setTasks] = useState([
+    let [tasks, setTasks] = useState({
+        [todolistID1]:[
         {id: v1(), title: "HTML&CSS", isDone: true},
         {id: v1(), title: "JS", isDone: true},
         {id: v1(), title: "ReactJS", isDone: false},
         {id: v1(), title: "Rest API", isDone: false},
         {id: v1(), title: "GraphQL", isDone: false},
-    ]);
+    ],
+        [todolistID2] : [
+            {id: v1(), title: "HTML&CSS2", isDone: true},
+            {id: v1(), title: "JS2", isDone: true},
+            {id: v1(), title: "ReactJS2", isDone: false},
+            {id: v1(), title: "Rest API2", isDone: false},
+            {id: v1(), title: "GraphQL2", isDone: false},
+            
+        ]
+});
    // let [filter, setFilter] = useState<FilterValuesType>("all");
 
 
