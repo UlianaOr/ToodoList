7be2c3f6fd6059483cde42818b51ae354af5996b
UlianaOr <Ouliana9@gmail.com>
2022-10-06 10:07:02 +0200

    todoPerfect

--- src/components/input.tsx
+++ src/components/input.tsx
@@ -2,9 +2,10 @@ import { type } from "os";
 import React, { KeyboardEvent,ChangeEvent, useState } from "react";
 
  type InputPropsType = {
-    callBack: (newTitle:string)
+    callBack: (newTitle:string)=> void
+}
 
- }
+ 
 
 export const Input =(props:InputPropsType) => {
     let [title, setTitle] = useState("")
@@ -16,8 +17,9 @@ export const Input =(props:InputPropsType) => {
     }
 
     const addTask = () => {
+        let newTitle = title.trim();
         if (title.trim() !== "") {
-            props.callBack(newTitle);
+            props. callBack (newTitle);
             setTitle("");
         } else {
             setError("Title is required");
