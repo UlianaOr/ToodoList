94a2c87e444b92f349e9b98496d1c04e308a51d7
UlianaOr <Ouliana9@gmail.com>
2022-10-11 17:51:25 +0200

    Updates

---
+++ precache-manifest.f0f8751d3344e1c6da3cd13e0b70ed53.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "be8b45f999938b6823d60a6d71eedcba",
+    "url": "/ToodoList/index.html"
+  },
+  {
+    "revision": "d5901463f20aefa798ee",
+    "url": "/ToodoList/static/css/main.d4474395.chunk.css"
+  },
+  {
+    "revision": "f9580a088ad2273be431",
+    "url": "/ToodoList/static/js/2.fd654bc5.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/ToodoList/static/js/2.fd654bc5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d5901463f20aefa798ee",
+    "url": "/ToodoList/static/js/main.efc0b9af.chunk.js"
+  },
+  {
+    "revision": "917893751b962bc2f59e",
+    "url": "/ToodoList/static/js/runtime-main.44bff140.js"
+  }
+]);
