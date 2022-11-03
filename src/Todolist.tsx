ab972e6daff0d7adf1bff3a89127102715ecc9f0
UlianaOr <Ouliana9@gmail.com>
2022-11-03 16:16:22 +0100

    Buttons

--- src/Todolist.tsx
+++ src/Todolist.tsx
@@ -2,7 +2,7 @@ import React, {ChangeEvent} from 'react';
 import {FilterValuesType} from './App';
 import {AddItemForm} from './AddItemForm';
 import {EditableSpan} from './EditableSpan';
-import { IconButton } from '@material-ui/core';
+import { Button, IconButton } from '@material-ui/core';
 import { Delete } from '@material-ui/icons';
 
 export type TaskType = {
@@ -75,7 +75,7 @@ export function Todolist(props: PropsType) {
             }
         </ul>
         <div>
-            <button className={props.filter === 'all' ? "active-filter" : ""}
+           {/*  <button className={props.filter === 'all' ? "active-filter" : ""}
                     onClick={onAllClickHandler}>All
             </button>
             <button className={props.filter === 'active' ? "active-filter" : ""}
@@ -83,7 +83,16 @@ export function Todolist(props: PropsType) {
             </button>
             <button className={props.filter === 'completed' ? "active-filter" : ""}
                     onClick={onCompletedClickHandler}>Completed
-            </button>
+            </button> */}
+            <Button variant={props.filter === 'all' ? "outlined" : "contained"} color="primary" size="small" onClick={onAllClickHandler}>
+            All
+           </Button>
+            <Button variant={props.filter === 'active' ? "outlined" : "contained"} color="secondary" size="small" onClick={onActiveClickHandler}>
+            Active
+            </Button>
+            <Button variant={props.filter === 'completed' ? "outlined" : "contained"} color="primary" size="small" onClick={onCompletedClickHandler}>
+            Completed
+              </Button>
         </div>
     </div>
 }
