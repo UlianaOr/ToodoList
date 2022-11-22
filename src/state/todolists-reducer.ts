247b16f8ffa07ac601643ddb1347ff44c6f26e10
UlianaOr <Ouliana9@gmail.com>
2022-11-22 14:40:54 +0100

    test3

--- src/state/todolists-reducer.ts
+++ src/state/todolists-reducer.ts
@@ -7,15 +7,24 @@ type ActionType = {
 
 export const todolistsReducer= (state: Array <TodolistType>, action: ActionType) => {
     switch(action.type) {
-        case 'XXX':
-           return state;
+        case 'REMOVE-TODOLIST':{
+
+            //setTodolists(todolists.filter(tl => tl.id != id));
+            // удалим таски для этого тудулиста из второго стейта, где мы храним отдельно таски
+            //delete tasks[id]; // удаляем св-во из объекта... значением которого являлся массив тасок
+            // засетаем в стейт копию объекта, чтобы React отреагировал перерисовкой
+           // setTasks({...tasks});
+           return state.filter (el=>el.id!==action.payload.todolistId1)
+        }
         default:
-            throw new Error ("I dont understand this type")
+            return state
 
 
     }
 
 }
+type tsarType=removeTodolistACType
+type removeTodolistACType=ReturnType<typeof removeTodolistAC>
 export const removeTodolistAC=(todolistId1:string)=>{
     return{
         type: 'REMOVE-TODOLIST', 
